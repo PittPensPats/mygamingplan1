f4a9d969ad0167c0b8b77984b06ec31460c845a7
PittPensPats <brittanynbarry@gmail.com>
2015-04-14 20:38:21 -0400

    testing login commit

--- main.py
+++ main.py
@@ -14,25 +14,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import os
+# import os
+# import webapp2
+
+# from google.appengine.api import users
+# from google.appengine.ext.webapp import template
+
+# def render_template(handler,templatename,templatevalues):
+  # path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
+  # html = template.render(path, templatevalues)
+  # handler.response.out.write(html)
+
+# class MainHandler(webapp2.RequestHandler):
+    # def get(self):
+	  # user = users.get_current_user()
+	  # if user:
+        # #render_template(self, 'index.html', {}) 
+		# #self.response.out.write('<html><body>yay you are logged in</body></html>')
+		# greeting = ('Welcome, %, (<a href="%s">sign out</a>)' % (user.nickname(), users.create_logout_url('/')))
+	  # else:
+	    # #self.response.out.write('<html><body><a href="%s">Sign in or register.</a></body></html>' % users.create_login_url('/')) 
+	    # greeting = ('<a href="%s">Sign in or register</a>.' % users.create_login_url('/'))
+    # self.response.out.write('<html><body>%s</body></html>' % greeting)
+	
+	
+# app = webapp2.WSGIApplication([
+    # ('/', MainHandler)
+# ], debug=True)
+
 import webapp2
+from google.appengine.api import users
 
-from google.appengine.ext.webapp import template
-
-def render_template(handler,templatename,templatevalues):
-  path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
-  html = template.render(path, templatevalues)
-  handler.response.out.write(html)
-
-class MainHandler(webapp2.RequestHandler):
+class MyHandler(webapp2.RequestHandler):
     def get(self):
-	  user = users.get_current_user()
-	  if user:
-        render_template(self, 'index.html', {})
-	  else:
-	    self.response.out.write('<html><body><a href="%s">Sign in or register.</a></body></html>' % users.create_login_url('/')) 
-	    
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        self.response.out.write("<html><body>%s</body></html>" % greeting)
+
+application = webapp2.WSGIApplication([('/', MyHandler),], debug=True)
